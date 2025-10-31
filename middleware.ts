fd054c5ed20dba4ec5a7f9cfb5426c176c924330
harshalx1dev <harshalx1.dev@gmail.com>
2025-10-31 23:53:15 +0530

    cookie log added

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,9 @@ const SESSION_API_ROUTE = `${process.env.SERVER_URL}/api/auth/session`;
 const getSession = async (request: NextRequest) => {
   const cookies = request.headers.get('cookie');
 
+  console.log('[COOKIES]', cookies);
+  console.log('[URL]', request.nextUrl);
+
   if (!cookies) return { userId: null, status: 401 };
 
   try {
