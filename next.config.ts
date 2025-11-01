4cbcd73799a209cdc11f02b8f4698d8b155508a1
Harshal <harshal.malkan@deptagency.com>
2025-11-02 01:26:10 +0530

    asset prefix removed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: '/',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
