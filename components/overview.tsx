8af8e0e53cb8f5a1ab4993cd77b51824a3ee4d60
harshalx1dev <harshalx1.dev@gmail.com>
2025-11-02 18:13:30 +0530

    foreground update

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 interface DataItem {
@@ -13,13 +12,12 @@ interface OverviewProps {
 }
 
 export const Overview = ({ data }: OverviewProps) => {
-  const { theme } = useTheme();
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
         <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={false} axisLine={false} />
         <YAxis stroke="#888888" fontSize={12} tickLine={false} tickFormatter={(value) => `$${value}`} axisLine={false} />
-        <Bar dataKey="total" fill={theme === 'dark' ? '#fefeff' : '#100b16'} radius={[4, 4, 0, 0]}  />
+        <Bar dataKey="total" fill="var(--foreground)" radius={[4, 4, 0, 0]}  />
       </BarChart>
     </ResponsiveContainer>
   )
